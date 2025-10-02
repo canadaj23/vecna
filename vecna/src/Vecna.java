6de37cda1971a2ffa7ffc87503367b1d08a15cce
Jamie Canada <122479314+canadaj23@users.noreply.github.com>
2025-10-02 10:08:12 -0400

    Delete vecna directory

--- vecna/src/Vecna.java
+++
@@ -1,9 +0,0 @@
-import com.chess.engine.board.Board;
-
-public class Vecna {
-    public static void main(String[] args) {
-        Board board = Board.CreateInitialBoard();
-
-        System.out.println(board);
-    }
-}
